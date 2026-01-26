ed22ffdbb9918c5126dcdc6f9ce2bf68f42af2b4
Проскуряков денис Олегович <darhan@MacBook-Air-Proskurakov.local>
2026-01-26 00:06:10 -0800

    fix

---
+++ task_manager_app/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "task_manager_app.apps.TaskManagerAppConfig"
--- task_manager_app/apps.py
+++ task_manager_app/apps.py
@@ -1,5 +1,43 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.contrib.auth.hashers import make_password
+
+
+def create_users(sender, **kwargs):
+    from django.contrib.auth.models import User
+
+    if User.objects.exists():
+        return
+
+    users = [
+        {
+            "username": "admin",
+            "password": "admin",
+            "is_superuser": True,
+            "is_staff": True,
+        },
+        {
+            "username": "user1",
+            "password": "password",
+        },
+        {
+            "username": "user2",
+            "password": "password",
+        },
+    ]
+
+    for user in users:
+        User.objects.create(
+            username=user["username"],
+            password=make_password(user["password"]),
+            is_superuser=user.get("is_superuser", False),
+            is_staff=user.get("is_staff", False),
+        )
 
 
 class TaskManagerAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "task_manager_app"
+
+    def ready(self):
+        post_migrate.connect(create_users, sender=self)
